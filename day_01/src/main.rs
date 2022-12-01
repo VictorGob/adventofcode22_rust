cc00bbc3ad66077d735f4150f47e65ebda642e16
VictorGob <vgordaliza@gmail.com>
2022-12-01 16:24:51 +0100

    Minor change

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let filename = "01.txt";
+    let filename = "input.txt";
     let mut cal_count: Vec<u32> = Vec::new();
     if let Ok(lines) = read_lines(filename) {
         let mut count: u32 = 0;
@@ -18,11 +18,11 @@ fn main() {
                 }
             } 
         }
+        // When lines ends, add last count
         cal_count.push(count);
     } 
     // Sort and reverse vector
     cal_count.sort_by(|a, b| b.cmp(a));
-    // println!("Res: {:?}", cal_count);
     println!("Elf carrying the most Calories: {}", cal_count[0]);
     let top3_cal: u32 = (cal_count[0..3]).to_vec().iter().sum();
     println!("How many Calories are those Elves carrying in total?: {}", top3_cal);
