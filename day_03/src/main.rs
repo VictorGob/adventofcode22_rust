1ffb4affe741fec0037626222bdaecfcd5f36327
VictorGob <vgordaliza@gmail.com>
2022-12-07 00:22:27 +0100

    Added day 3, task 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,9 +1,8 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-
-use itertools::Itertools;
 
 fn main() {
     let filename: &str = "input_d3.txt";
@@ -33,19 +32,52 @@ fn main() {
     println!("Count: {}", count);
 
     /* Part 2 */
-    // From https://fasterthanli.me/series/advent-of-code-2022/part-3#part-2
-    let mut count: u32 = 0;
-    let mut grouplines: Vec<&str> = Vec::new();
-    match read_lines(filename) {
-        Ok(lines) => lines.chunks(3).into_iter().for_each(|group| {
-            for line in group {
-                let no_rep = line.into_iter().unique();
-                // println!("{:?}", line.unwrap());
-                println!("{:?}", no_rep);
+    // From https://github.com/ZAZPRO/adventofcode-2022/blob/main/03/rust/src/bin/part_two.rs
+    let mut total_sum: u32 = 0;
+    let file = std::fs::read_to_string(filename).unwrap();
+    let lines: Vec<&str> = file.lines().collect();
+    for chunk in lines.chunks(3) {
+        // Transform strs into slices of bytes.
+        let first_member = chunk[0].as_bytes();
+        let second_member = chunk[1].as_bytes();
+        let third_member = chunk[2].as_bytes();
+
+        // Create a Set out of the first member.
+        let mut a: HashSet<u8> = HashSet::new();
+        for v in first_member {
+            a.insert(*v);
+        }
+        // Create a Set out of the second member.
+        let mut b: HashSet<u8> = HashSet::new();
+        for v in second_member {
+            b.insert(*v);
+        }
+
+        // Create a Set out of the third member.
+        let mut c: HashSet<u8> = HashSet::new();
+        for v in third_member {
+            c.insert(*v);
+        }
+
+        // B&C intersection vector.
+        let b_c_intersection: Vec<&u8> = b.intersection(&c).collect();
+        // Create a Set out of the second and third intersection.
+        let mut bc: HashSet<u8> = HashSet::new();
+        for v in b_c_intersection {
+            bc.insert(*v);
+        }
+
+        // Find intersection of three sets, that's our common item.
+        for v in a.intersection(&bc) {
+            // Calculate priority for each item and add it to the total sum.
+            if *v > 96 {
+                total_sum += u32::from(*v) - 96;
+            } else {
+                total_sum += u32::from(*v) - 38;
             }
-        }),
-        Err(p_err) => panic!("Error: {}", p_err),
+        }
     }
+    println!("Task 2: {}", total_sum)
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
