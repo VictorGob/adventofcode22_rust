55ccc6cd4b8052400a8a088ce47d9a9c77b6cdc2
VictorGob <vgordaliza@gmail.com>
2022-12-11 01:01:05 +0100

    Day 5: WIP. Correct stacks generation

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -5,24 +5,37 @@ fn main() -> Result<(), io::Error> {
     let file = std::fs::read_to_string(filename).unwrap();
     let lines: Vec<&str> = file.lines().collect();
 
-    let mut ship: Vec<Vec<&str>> = Vec::new();
+    // See https://www.reddit.com/r/rust/comments/ad74ls/a_vector_made_of_vectors/
+    let mut ship: Vec<Vec<char>> = Vec::new();
+    // Read the ship's stacks
     for line in lines {
+        // Stop if no brackets are found
         if !line.contains('[') {
             break;
         }
         let fill_line = line.replace("    ", " [_] ");
         // println!("{}", fill_line);
         let split_line: Vec<&str> = fill_line.split_whitespace().collect();
-        // println!("{:?}", split_line);
+        // println!("split_line: {:?}", split_line);
         // println!("{}", ship.len());
-        
+
         if ship.is_empty() {
             // Initialize ship vector
             for crt in split_line {
-                println!("{}", crt.chars().collect::<Vec<char>>()[1]);
+                // println!("{}", crt.chars().collect::<Vec<char>>()[1]);
+                ship.push(vec![crt.chars().collect::<Vec<char>>()[1]]);
             }
-
+            continue;
         }
+
+        let mut mut_ship = ship.iter_mut();
+        for crt in split_line {
+            let current_stack = mut_ship.next().unwrap();
+            current_stack.push(crt.chars().collect::<Vec<char>>()[1]);
+        }
+    }
+    for line in &ship {
+        println!("{:?}", line);
     }
     Ok(())
 }
